89aaaca2c419803b78a295530ad25101b50ebf7c
Jonath1n <jonathanvelascob40@gmail.com>
2025-10-14 18:15:16 -0500

    Add files via upload

---
+++ Ejercicio_1.java
@@ -0,0 +1,37 @@
+package secuenciales;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Jonathan Adrián 
+ */
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Crear variables
+        String username="Camila";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+        //Proceso
+        sumaDulces=cofreUno + cofreDos;
+        
+        //Salida
+        System.out.print(username + " ahora tiene un total de " + sumaDulces + "caramelos magicos");
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+}
